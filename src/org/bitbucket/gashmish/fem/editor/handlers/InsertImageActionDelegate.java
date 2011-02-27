bdfd9f11452f96069f8fa6694962373e9d068f28
pavele <none@none>
2011-02-27 17:30:29 +0300

    Add class for insert images.

---
+++ src/org/bitbucket/gashmish/fem/editor/handlers/InsertImageActionDelegate.java
@@ -0,0 +1,32 @@
+package org.bitbucket.gashmish.fem.editor.handlers;
+
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IEditorActionDelegate;
+import org.eclipse.ui.IEditorPart;
+
+public class InsertImageActionDelegate implements IEditorActionDelegate {
+
+	public InsertImageActionDelegate() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void setActiveEditor(IAction action, IEditorPart targetEditor) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void run(IAction action) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void selectionChanged(IAction action, ISelection selection) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
